d77f0afcb23a06c6da01003602e25c78a48695aa
Gorani77 <sp2877@naver.com>
2025-08-14 08:55:03 +0900

    feat: home 페이지 내용 삭제

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.module.scss';
+
+const Home: React.FC = () => {
+
+  return (
+    <div className={styles.homeContainer}>
+    </div>
+  );
+};
+
+export default Home;
