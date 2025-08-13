e5087183b453b0129891d3f75ac4b7b04b1f1e2e
Gorani77 <sp2877@naver.com>
2025-08-13 09:36:47 +0900

    feat: 리더보드 범례 제거

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -116,7 +116,7 @@ const Leaderboard: React.FC = () => {
                       }))}
                     keys={['score']}
                     indexBy="model"
-                    margin={{ top: 50, right: 130, bottom: 100, left: 60 }}
+                    margin={{ top: 30, right: 30, bottom: 100, left: 60 }}
                     padding={0.3}
                     valueScale={{ type: 'linear' }}
                     indexScale={{ type: 'band', round: true }}
@@ -166,30 +166,7 @@ const Leaderboard: React.FC = () => {
                     labelSkipWidth={12}
                     labelSkipHeight={12}
                     labelTextColor="#ffffff"
-                    legends={[
-                      {
-                        dataFrom: 'indexes',
-                        anchor: 'bottom-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 120,
-                        translateY: 0,
-                        itemsSpacing: 2,
-                        itemWidth: 100,
-                        itemHeight: 20,
-                        itemDirection: 'left-to-right',
-                        itemOpacity: 0.85,
-                        symbolSize: 20,
-                        effects: [
-                          {
-                            on: 'hover',
-                            style: {
-                              itemOpacity: 1
-                            }
-                          }
-                        ]
-                      }
-                    ]}
+                    legends={[]}
                     role="application"
                     ariaLabel="Nivo bar chart demo"
                     barAriaLabel={function(e){return e.id+": "+e.formattedValue+" in model: "+e.indexValue}}
